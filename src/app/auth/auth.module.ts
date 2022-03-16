873c17332bb0b0cca755e1f245f77058e8861f15
niiwade <niilantewade77@gmail.com>
2022-03-16 22:35:07 +0000

    added ngrx

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './components/register/register.component';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 const routes = [
   {
@@ -15,7 +16,7 @@ const routes = [
     RegisterComponent
   ],
   imports: [
-    CommonModule, RouterModule.forChild(routes),
+    CommonModule, RouterModule.forChild(routes), ReactiveFormsModule
   ]
 })
 export class AuthModule { }
